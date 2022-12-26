62525c66e138a00bc3502e8d7985323aabefa18d
Prakash38764 <121441719+Prakash38764@users.noreply.github.com>
2022-12-26 19:18:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var number = 5; //
+/*
+vslfnvkslnvfs
+*/  
+var myname = "Beau"
+myName = 8
+
+let ouName = "freecodecamp"
+
+const pi = 3.14
+
+var a;
+var b = 5;
+
+a = 7
+b = a
+
+console.log(a)
